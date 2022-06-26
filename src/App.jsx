fd7d6eede631691331699cb15aea1c4f686bb5db
kyleCS50 <kyledanielfrancis@gmail.com>
2022-06-26 14:08:59 +1200

    Added styles

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Portfolio from "./components/portfolio/Portfolio";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
