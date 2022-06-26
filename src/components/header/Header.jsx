66811cebf8ddee8d1649aac2ff31c28f3633d8ea
kyleCS50 <kyledanielfrancis@gmail.com>
2022-06-26 15:50:54 +1200

    Worked on header page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from "./HeaderSocials";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header_container">
+        <h5>Hello I am</h5>
+        <h1>Kyle Francis</h1>
+        <h5 className="text-light">Software Engineer</h5>
+        <CTA />
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
 
-export default Header
+        <a href="#contact" className="scroll_down">Scroll Down</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
